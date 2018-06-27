56111056e07be955b7aa05b1cdb2d0ad0f2e3c5e
Dmytro Yashkir <dima@dyashkir.com>
2018-06-27 14:39:28 +0900

    start

---
+++ config.fish
@@ -0,0 +1,5 @@
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "~/Qt/Qt/5.11.0/clang_64/bin" $fish_user_paths
+set -g fish_user_paths "/Users/dyashkir/anaconda3/bin" $fish_user_paths
+
+source /Users/dyashkir/anaconda3/etc/fish/conf.d/conda.fish
